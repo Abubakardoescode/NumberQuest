e6c0756c1b31eb5c952216954f0a25332ba8d771
Abu Bakar Siddique <91130028+Abubakardoescode@users.noreply.github.com>
2024-03-22 10:16:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import random as rd
 
 def number_quest():
     st.title("Number Quest")
-    st.text("Welcome to 'Number Quest' Can you figure out the secret number hidden between 1 and 50? You have 5 attempts to guess it right. Enter your guess in the box below and click "Submit" to see if you've cracked the code. Good luck!")
+    st.text("Welcome to 'Number Quest' Can you figure out the secret number hidden between 1 and 50? You have 5 attempts to guess it right. Enter your guess in the box below and click 'Submit' to see if you've cracked the code. Good luck!")
 
     # Initialize session state
     if 'attempts_left' not in st.session_state:
